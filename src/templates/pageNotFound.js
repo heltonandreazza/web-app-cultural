fbe0fe75b0770a77c64d05eeeb76a27c56ef982f
Helton Andreazza <heltonandreazza@Heltons-MacBook-Air.local>
2021-08-10 14:39:37 -0300

    fix css page 404

--- src/templates/pageNotFound.js
+++ src/templates/pageNotFound.js
@@ -2,19 +2,19 @@ import React from 'react'
 import logo from '../assets/logo.png'
 import { getWhatsUrl } from '../utils'
 import { CTA_WA_MESSAGES, URLS } from '../utils/constants'
+import '../styles/global.css'
 
 const PageNotFound = () => (
   <div className="min-h-screen pt-16 pb-12 flex flex-col bg-white">
     <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex-shrink-0 flex justify-center">
         <a href="/" className="inline-flex">
-          <span className="sr-only">Logo</span>
-          <img className="h-12 w-auto" src={logo} alt="" />
+          <img className="h-12 w-auto" src={logo} alt="logo" />
         </a>
       </div>
       <div className="py-16">
         <div className="text-center">
-          <p className="text-sm font-semibold text-green-600 uppercase tracking-wide">404 error</p>
+          <p className="text-sm font-semibold text-green-600 uppercase tracking-wide">Erro 404</p>
           <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
             Página não encontrada.
           </h1>
