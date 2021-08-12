db6670b30fbfcc08a8b81ebcfb0b12515199d845
Helton Andreazza <heltonandreazza@Heltons-MacBook-Air.local>
2021-08-12 17:13:47 -0300

    fix post

--- src/templates/post.js
+++ src/templates/post.js
@@ -109,11 +109,6 @@ const Post = ({ data: { post, posts } = {}, path }) => {
                 {post?.title}
               </span>
             </h1>
-            <p className="mt-8 text-xl text-gray-500 leading-8">
-              Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi,
-              nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci
-              at in viverra scelerisque eget. Eleifend egestas fringilla sapien.
-            </p>
             <div>{documentToReactComponents(contentRichJson, buildOptions(path))}</div>
           </div>
         </div>
